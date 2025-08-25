776418868f7a4fa254a62fd2d8673d485634fb5c
Colinpiguet <colin.piguet@edu.tbz.ch>
2025-08-25 08:38:40 +0200

    update

--- environment/src/grundlagen/PriceCalculator.java
+++ environment/src/grundlagen/PriceCalculator.java
@@ -6,20 +6,17 @@ public class PriceCalculator {
         double addon_discount;
         double result;
 
-        // Rabatt für Zusatzausstattungen
-        if (extras >= 5) 
+        if (extras >= 5)
             addon_discount = 15;
         else if (extras >= 3)
             addon_discount = 10;
         else 
             addon_discount = 0;
 
-        // Vergleich mit Händlerrabatt
         if (discount > addon_discount)
             addon_discount = discount;
 
-        // Endpreis berechnen
-        result = baseprice / 100.0 * (100 - discount) 
+        result = baseprice / 100.0 * (100 - discount)
                + specialprice 
                + extraprice / 100.0 * (100 - addon_discount);
 
